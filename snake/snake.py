3534b12c57cfab553cae5467f4066d4e0690308b
Niel Conradie <nielconradie93@gmail.com>
2022-07-08 16:12:06 +0200

    Update snake.py
    Object-Oriented Programming Implemetation.

--- snake/snake.py
+++ snake/snake.py
@@ -1,53 +1,39 @@
 import pygame
 
-from pygame.locals import *
 
+class Snake:
+    """A class to represent a snake."""
 
-def draw_snake():
-    """Display snake."""
-    rgb = (0, 0, 0)
-    surface.fill(rgb)
-    surface.blit(snake, (snake_x, snake_y))
-    pygame.display.flip()
+    def __init__(self, screen):
+        """Initialize class attributes."""
+        self.screen = screen
+        self.image = pygame.image.load("assets/snake.jpg").convert()
+        self.x = 600
+        self.y = 320
 
+    def move_left(self):
+        """Move snake left."""
+        self.x -= 40
+        self.draw()
 
-if __name__ == "__main__":
-    pygame.init()
+    def move_right(self):
+        """Move snake right."""
+        self.x += 40
+        self.draw()
 
-    # Display screen.
-    pygame.display.set_caption("Snake")
-    surface = pygame.display.set_mode((1280, 720))
-    rgb = (0, 0, 0)
-    surface.fill(rgb)
+    def move_up(self):
+        """Move snake up."""
+        self.y -= 40
+        self.draw()
 
-    # Import snake image.
-    snake = pygame.image.load("snake/assets/snake.jpg").convert()
-    snake_x, snake_y = 600, 320
-    surface.blit(snake, (snake_x, snake_y))
-    pygame.display.flip()
+    def move_down(self):
+        """Move snake down."""
+        self.y += 40
+        self.draw()
 
-    # Move snake conditions.
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                # Quit game.
-                if event.key == K_ESCAPE:
-                    running = False
-
-                # Move snake around.
-                if event.key == K_LEFT:
-                    snake_x -= 40
-                    draw_snake()
-                if event.key == K_RIGHT:
-                    snake_x += 40
-                    draw_snake()
-                if event.key == K_UP:
-                    snake_y -= 40
-                    draw_snake()
-                if event.key == K_DOWN:
-                    snake_y += 40
-                    draw_snake()
-
-            elif event.type == QUIT:
-                running = False
+    def draw(self):
+        """Display snake."""
+        rgb = (0, 0, 0)
+        self.screen.fill(rgb)
+        self.screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()

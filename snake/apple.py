e11a29dceccd4a1808bf851207da7184a57ff2e2
Niel Conradie <nielconradie93@gmail.com>
2022-07-11 15:59:38 +0200

    Update apple.py
    Move apple on screen method.

--- snake/apple.py
+++ snake/apple.py
@@ -1,5 +1,7 @@
 import pygame
 
+from random import randint
+
 
 class Apple:
     """A class to represent an apple."""
@@ -15,3 +17,8 @@ class Apple:
         """Display the apple."""
         self.screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
+
+    def move(self):
+        """Move the apple to random position."""
+        self.x = randint(10, 15) * 40
+        self.y = randint(10, 15) * 40
